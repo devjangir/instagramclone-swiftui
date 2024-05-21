fc8a6340ceb15745b72dfafa9b3ff177b2a45d2a
devjangir <devpassion7@gmail.com>
2024-05-21 22:47:16 +0530

    PhotoUI picker added

--- InstagramClone/Core/UploadPost.swift
+++ InstagramClone/Core/UploadPost.swift
@@ -6,13 +6,71 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct UploadPost: View {
+    @State private var caption: String = ""
+    @State private var isPhotoPresented = false
+    @State private var photoPickerItem: PhotosPickerItem?
+    @StateObject private var viewModel = UploadPostViewModel()
+    @Binding var tabIndex: Int
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Button {
+                    print("cancel picker")
+                    caption = ""
+                    viewModel.selectedImage = nil
+                    viewModel.profileImage = nil
+                    tabIndex = 0
+                } label: {
+                    Text("Cancel")
+                }
+                Spacer()
+                
+                Text("New Post")
+                    .fontWeight(.semibold)
+                
+                Spacer()
+                Button {
+                    print("upload picker")
+                    isPhotoPresented = true
+                } label: {
+                    Text("Upload")
+                        .fontWeight(.semibold)
+                }
+            }
+            .padding()
+            
+            
+            // Post Image and Caption
+            HStack {
+                if let image = viewModel.profileImage {
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 100)
+                        .clipped()
+                }
+                
+                TextField("Enter your caption", text: $caption, axis: .vertical)
+                    
+            }
+            
+            Spacer()
+            
+        }
+        .onAppear {
+            isPhotoPresented.toggle()
+        }
+        .photosPicker(isPresented: $isPhotoPresented, selection: $viewModel.selectedImage)
+        .photosPickerStyle(.inline)
+        
+        
+        
     }
 }
 
 #Preview {
-    UploadPost()
+    UploadPost(tabIndex: .constant(0))
 }
--- InstagramClone/Core/UploadPostViewModel.swift
+++ InstagramClone/Core/UploadPostViewModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import PhotosUI
+import SwiftUI
+
+@MainActor
+class UploadPostViewModel: ObservableObject {
+    @Published var selectedImage: PhotosPickerItem? {
+        didSet {
+            Task {
+                await loadImage(fromItem: selectedImage)
+            }
+        }
+    }
+    
+    @Published var profileImage: Image?
+    func loadImage(fromItem item: PhotosPickerItem?) async {
+        guard let item else {
+            return
+        }
+        guard let image = try? await item.loadTransferable(type: Image.self) else { return }
+        // guard let uiImage = UIImage(data: data) else { return }
+        profileImage = image// Image(uiImage: uiImage)
+    }
+}
