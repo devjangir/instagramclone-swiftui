71a218fb62a2a1d6543d3664705e218f405adb60
devjangir <devpassion7@gmail.com>
2024-05-21 22:46:23 +0530

    upload post acivity added

---
+++ InstagramClone/Core/UploadPost.swift
@@ -0,0 +1,18 @@
+//
+//  UploadPost.swift
+//  InstagramClone
+//
+//  Created by djangir on 21/05/24.
+//
+
+import SwiftUI
+
+struct UploadPost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UploadPost()
+}
---
+++ InstagramClone/Core/UploadPostViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UploadPostViewModel.swift
+//  InstagramClone
+//
+//  Created by djangir on 21/05/24.
+//
+
+import Foundation
